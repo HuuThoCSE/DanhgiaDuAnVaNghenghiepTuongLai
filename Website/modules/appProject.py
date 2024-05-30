4286d8dc4e0694baac6ddb7dcfab8f17b9d9bb37
HuuThoCSE <huutho.cse@gmail.com>
2024-05-30 14:58:21 +0700

    Update

--- Website/modules/appProject.py
+++ Website/modules/appProject.py
@@ -12,16 +12,14 @@ mydb = mysql.connector.connect(
 )
 
 # Định nghĩa route trong module
-@appProject.route('/<int:project_id>')
+@appProject.route('/<int:project_id>', methods=['GET', 'POST'])
 def infoProject(project_id):
     if 'loggedin' not in session:
         return redirect(url_for('appAuth.Login'))
     if 'idPerm' not in session:
         return "Bạn không có quyền vào trang này. Nếu lỗi liên hệ admin."
-    
 
     if request.method == 'POST':
-        project_id = request.form.get('project_id')
         student_code = session.get('student_code')
         milestone_name = request.form.get('milestone_name')
         milestone_description = request.form.get('milestone_description')
@@ -36,7 +34,7 @@ def infoProject(project_id):
         mycursor.execute(query, values)
         mydb.commit()
 
-        return redirect(url_for('project_page', project_id=project_id))
+        return redirect(url_for('appProject.infoProject', project_id=project_id))
 
     mycursor = mydb.cursor()
     query = """
@@ -49,13 +47,26 @@ def infoProject(project_id):
             LEFT JOIN Courses d ON d.course_code = b.course_code
             LEFT JOIN Teachers e ON e.teacher_code = a.teacher_code
             where project_id = %(project_id)s
+            
         """
     mycursor.execute(query, {'project_id': project_id})
     data = mycursor.fetchone()
     
     print(data)
+
+    query = """
+            SELECT ProjectProgress_id,project_id, student_code, milestone_name, milestone_description, DATE_FORMAT(last_updated, '%d-%m-%Y') 
+            FROM ProjectProgress 
+            where project_id = %(project_id)s
+            ORDER BY last_updated DESC
+            """
+    
+    mycursor.execute(query, {'project_id': project_id})
+
+    project_list = mycursor.fetchall()
     
     mycursor.close()
     return render_template('Project/project_dashboard.html', 
                            response=data,
-                           idPerm=session.get('idPerm'),)
+                           idPerm=session.get('idPerm'),
+                           project_list=project_list)
